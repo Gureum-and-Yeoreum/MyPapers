d8eed6ece32a1daef7920d7cea839008eeffe787
Gureum-and-Yeoreum <yeoreumcloud@gmail.com>
2023-06-21 16:10:40 +0900

    hgh

--- auth_test2.py
+++
@@ -1,9 +0,0 @@
-from mendeley import Mendeley
-
-# These values should match the ones supplied when registering your application.
-mendeley = Mendeley('15565', client_secret="83eGkxeG3I5COtC9")
-
-auth = mendeley.start_client_credentials_flow()
-session = auth.authenticate()
-
-print session.profiles.me.display_name
